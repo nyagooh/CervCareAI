923e1e08bbcafca2250aa62ef810758552da45de
nyagooh <annmaina614@gmail.com>
2025-06-20 15:03:19 +0300

    import component hero to page.tsx

--- my-frontend/src/app/page.tsx
+++ my-frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-pink-50 via-purple-50 to-rose-50">
       <Navbar />
+      <Hero />
        </div>
   )
 }
