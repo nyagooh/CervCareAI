009c1e15fa1dd734fd528f00e786921de75f18de
doonyango <datieno001@gmail.com>
2025-06-23 10:36:48 +0300

    chore: setup HowItWorks component with client directive and required imports

---
+++ my-frontend/src/app/components/HowItWorks.tsx
@@ -0,0 +1,4 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { Heart, FileText, Brain, Sparkles, ArrowRight, Shield, Stethoscope, Users, Calendar, CheckCircle } from 'lucide-react';
